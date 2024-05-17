aa42cab4b0448ac67b17cd24032514d916c36cdd
thewebdev2213 <thewebdev.2213@gmail.com>
2024-05-17 22:09:25 +0530

    Create job delete API.

--- src/app/api/job/route.ts
+++ src/app/api/job/route.ts
@@ -180,3 +180,40 @@ export const GET = async (req: NextResponse) => {
     );
   }
 };
+
+export const DELETE = async (req: NextResponse) => {
+  const session = await getServerSession(authOptions);
+  if (!session?.user?._id) {
+    return NextResponse.json({
+      message: 'Unauthorized',
+      status: 401,
+    });
+  }
+
+  try {
+    const { job_id } = await req.json();
+    const results = await Job.findByIdAndDelete(
+      new mongoose.Types.ObjectId(job_id),
+    );
+    if (!results) {
+      return NextResponse.json({
+        status: 404,
+        message: 'Job not found',
+      });
+    } else {
+      return NextResponse.json({
+        status: 200,
+        data: results,
+        message: 'Job deleted successfully',
+      });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occurred while delete job.',
+        error: error,
+      },
+      { status: 500 },
+    );
+  }
+};
