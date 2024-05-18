10fb3ab33c3cc97a1e512e67ed244778bdda6bde
thewebdev2213 <thewebdev.2213@gmail.com>
2024-05-18 22:43:11 +0530

    Create job details API.

---
+++ src/app/api/job-details/route.ts
@@ -0,0 +1,113 @@
+'use server';
+import Job from '@/models/job';
+import mongoose from 'mongoose';
+import { connect } from '@/db/mongodb';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/service/AuthOptions';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const session = await getServerSession(authOptions);
+  if (!session?.user?._id) {
+    return NextResponse.json({
+      message: 'Unauthorized',
+      status: 401,
+    });
+  }
+
+  try {
+    await connect();
+    const { job_id } = await req.json();
+
+    const jobDetails = await Job.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(job_id),
+        },
+      },
+      {
+        $lookup: {
+          from: 'jobapplications',
+          localField: '_id',
+          foreignField: 'job_id',
+          as: 'applicants',
+        },
+      },
+      {
+        $lookup: {
+          from: 'states',
+          localField: 'state',
+          foreignField: '_id',
+          as: 'state_info',
+        },
+      },
+      {
+        $lookup: {
+          from: 'countries',
+          localField: 'country',
+          foreignField: '_id',
+          as: 'country_info',
+        },
+      },
+      {
+        $lookup: {
+          from: 'cities',
+          localField: 'city',
+          foreignField: '_id',
+          as: 'city_info',
+        },
+      },
+      {
+        $lookup: {
+          from: 'categories',
+          localField: 'skills',
+          foreignField: '_id',
+          as: 'skill_info',
+        },
+      },
+      {
+        $addFields: {
+          city: {
+            _id: { $arrayElemAt: ['$city_info._id', 0] },
+            name: { $arrayElemAt: ['$city_info.name', 0] },
+          },
+          state: {
+            _id: { $arrayElemAt: ['$state_info._id', 0] },
+            name: { $arrayElemAt: ['$state_info.name', 0] },
+          },
+          country: {
+            _id: { $arrayElemAt: ['$country_info._id', 0] },
+            name: { $arrayElemAt: ['$country_info.name', 0] },
+          },
+          skills: {
+            _id: { $arrayElemAt: ['$skill_info._id', 0] },
+            category: { $arrayElemAt: ['$skill_info.category', 0] },
+            subcategory: { $arrayElemAt: ['$skill_info.subcategory', 0] },
+          },
+        },
+      },
+      {
+        $project: {
+          city_info: 0,
+          skill_info: 0,
+          state_info: 0,
+          country_info: 0,
+        },
+      },
+    ]);
+
+    return NextResponse.json({
+      status: 200,
+      data: jobDetails,
+      message: 'Job get successfully',
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occurred while getting job details.',
+        error: error,
+      },
+      { status: 500 },
+    );
+  }
+};
