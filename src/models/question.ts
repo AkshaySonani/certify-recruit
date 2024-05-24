f185e3d7797eb4a721b400a6fcaf17329bc3d9bf
thewebdev2213 <thewebdev.2213@gmail.com>
2024-05-24 23:53:29 +0530

    Update question.ts

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,76 +1,53 @@
-// import mongoose from 'mongoose';
-// import { createModal } from '@/service/Helper';
-
-// const questionSchema = new mongoose.Schema({
-//   exam_id: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Exam',
-//     required: true,
-//   },
-//   questions: {
-//     type: [
-//       {
-//         text: {
-//           type: String,
-//           required: true,
-//         },
-//         choices: [String],
-//         correctAnswer: {
-//           type: String,
-//           select: false,
-//         },
-//       },
-//     ],
-//     required: true,
-//   },
-// });
-
-// questionSchema.set('timestamps', true);
-// export const Question = createModal('Question', questionSchema);
-// export default Question;
-
 import mongoose from 'mongoose';
 import { createModal } from '@/service/Helper';
 
-const questionSchema = new mongoose.Schema({
-  exam_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Exam',
-    required: true,
-  },
-  questions: {
-    type: [
+const questionSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+    },
+    ans: {
+      type: String,
+      select: false,
+      required: true,
+    },
+    category_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    option: [
       {
-        que_id: {
-          type: mongoose.Schema.Types.ObjectId,
-          default: mongoose.Types.ObjectId, // This generates an ObjectId automatically
-        },
-        question: {
-          type: String,
-          required: true,
-        },
-        questionType: {
-          type: String,
-          enum: ['mcq', 'true-false', 'short'],
-          required: true,
-        },
-        choices: {
-          type: Map,
-          of: String,
-          required: function () {
-            return this.questionType === 'mcq';
-          },
-        },
-        correctAnswer: {
-          type: String,
-          select: false,
-          required: true,
-        },
+        type: String,
+        required: true,
       },
     ],
-    required: true,
   },
-});
+  //   {
+  //   exam_id: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'Exam',
+  //     required: true,
+  //   },
+  //   questions: {
+  //     type: [
+  //       {
+  //         text: {
+  //           type: String,
+  //           required: true,
+  //         },
+  //         choices: [String],
+  //         correctAnswer: {
+  //           type: String,
+  //           select: false,
+  //         },
+  //       },
+  //     ],
+  //     required: true,
+  //   },
+  // }
+);
 
 questionSchema.set('timestamps', true);
 export const Question = createModal('Question', questionSchema);
