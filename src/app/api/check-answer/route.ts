8df1f7c7d45f86e55ed167eafab4650a272459c8
thewebdev2213 <thewebdev.2213@gmail.com>
2024-05-25 15:31:41 +0530

    Create check answer API.

---
+++ src/app/api/check-answer/route.ts
@@ -0,0 +1,75 @@
+'use server';
+import mongoose from 'mongoose';
+import { Question } from '@/models';
+import { connect } from '@/db/mongodb';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/service/AuthOptions';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const session = await getServerSession(authOptions);
+  if (!session?.user?._id) {
+    return NextResponse.json({
+      message: 'Unauthorized',
+      status: 401,
+    });
+  }
+
+  try {
+    await connect();
+    const { answers } = await req.json();
+
+    // Validate answers
+    if (!Array.isArray(answers) || answers.length === 0) {
+      return NextResponse.json({
+        status: 400,
+        message: 'Answers must be a non-empty array',
+      });
+    }
+
+    const questionIds = answers.map(
+      (answer) => new mongoose.Types.ObjectId(answer.que_id),
+    );
+    const questions = await Question.find({ _id: { $in: questionIds } }).select(
+      '+ans',
+    );
+
+    let correctAnswersCount = 0;
+
+    // Compare user's answers with the correct answers
+    answers?.forEach((userAnswer) => {
+      const question = questions?.find((q: any) =>
+        q?._id?.equals(userAnswer.que_id),
+      );
+      if (question && question?.ans === userAnswer?.ans) {
+        correctAnswersCount++;
+      }
+    });
+
+    // Calculate the percentage of correct answers
+    const totalQuestions = answers.length;
+    const correctPercentage = (correctAnswersCount / totalQuestions) * 100;
+
+    // Determine if the user passes
+    const pass = correctPercentage >= 70;
+
+    return NextResponse.json({
+      status: 200,
+      data: {
+        correctAnswersCount,
+        totalQuestions,
+        correctPercentage,
+        pass,
+      },
+      message: pass ? 'Congratulations' : 'Batter luck next time',
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occurred while getting questions.',
+        error: error,
+      },
+      { status: 500 },
+    );
+  }
+};
