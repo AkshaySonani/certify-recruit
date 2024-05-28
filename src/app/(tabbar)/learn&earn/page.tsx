5acc45e69d4994537f9453f8e25024d54ded7785
purvisutariya <purvisutariya01@gmail.com>
2024-05-28 23:47:55 +0530

    add learn&earn screen design

--- src/app/(tabbar)/learn&earn/page.tsx
+++ src/app/(tabbar)/learn&earn/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-import React, { Fragment } from "react";
-import { useRouter } from "next/navigation";
-import { TEXT } from "@/service/Helper";
+'use client';
+import React, { Fragment } from 'react';
+import { useRouter } from 'next/navigation';
+import { TEXT } from '@/service/Helper';
 
 const Page = () => {
   const router = useRouter();
@@ -9,19 +9,19 @@ const Page = () => {
   return (
     <div>
       <div>
-        <div className="text-meta-purple-1 font-semibold text-2xl mb-4">
+        <div className="mb-4 text-2xl font-semibold text-meta-purple-1">
           {TEXT?.LEARN_AND_EARN}
         </div>
-        <div className="bg-meta-gray-2 h-52 rounded-xl">
-          <div className="flex flex-col justify-center h-full items-center">
-            <p className="text-meta-purple-1 text-base">
+        <div className="h-52 rounded-xl bg-meta-gray-2">
+          <div className="flex h-full flex-col items-center justify-center">
+            <p className="text-base text-meta-purple-1">
               {
                 TEXT?.ONLY_WHEN_YOU_HAVE_CERTIFICATION_CAN_YOU_DO_LEARN_AND_EARN_USE
               }
             </p>
             <button
-              onClick={() => router?.push("/learn&earn/registrationInfo")}
-              className="bg-meta-blue-1 rounded-xl mt-5 text-white text-base px-5 py-3"
+              onClick={() => router?.push('/learn&earn/registrationInfo')}
+              className="mt-5 rounded-xl bg-meta-blue-1 px-5 py-3 text-base text-white"
             >
               {TEXT?.GO_TO_CERTIFICATION}
             </button>
