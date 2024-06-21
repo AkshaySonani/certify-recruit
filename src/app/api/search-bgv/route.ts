c8d1f53d9f01cb399f7ede0757ca9a5df5137e14
thewebdev2213 <163153761+thewebdev2213@users.noreply.github.com>
2024-06-21 22:15:27 +0530

    Create BGV search API.

---
+++ src/app/api/search-bgv/route.ts
@@ -0,0 +1,37 @@
+'use server';
+import { connect } from '@/db/mongodb';
+import { Individual, User } from '@/models';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/service/AuthOptions';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const session: any = await getServerSession(authOptions);
+  if (!session?.user?._id) {
+    return NextResponse.json({
+      message: 'Unauthorized',
+      status: 401,
+    });
+  }
+
+  try {
+    await connect();
+    const { email } = await req.json();
+
+    let user = await User.findOne({ email });
+    let results = await Individual.findOne({ user_ref_id: user?._id });
+
+    return NextResponse.json({
+      status: 200,
+      data: results?.bgv,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occurred while searching bgv.',
+        error: error,
+      },
+      { status: 500 },
+    );
+  }
+};
