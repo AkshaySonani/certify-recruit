c4a3a9f8a3bc2fded0702e3b098466e6681cbd52
purvisutariya <97298888+purvisutariya@users.noreply.github.com>
2024-07-15 15:54:47 +0530

    Fix Ui issue

--- src/Components/Autocomplete.tsx
+++ src/Components/Autocomplete.tsx
@@ -46,7 +46,7 @@ const AutoComplete = ({
           leaveTo="opacity-0"
           afterLeave={() => setQuery('')}
         >
-          <Combobox.Options className="absolute z-30 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+          <Combobox.Options className="absolute z-30 mt-1 max-h-44 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
             {filterArr?.length === 0 && query !== '' ? (
               <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
                 Nothing found.
